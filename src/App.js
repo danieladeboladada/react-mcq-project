db004a158633de96d8fd4d01b867fc708e60e21d
Daniel Adebola-Dada <daniel@cchubnigeria.com>
2021-05-12 09:41:23 -0400

    functioning quiz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { useState } from 'react';
+//import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
+const questions_arr = [
+  {
+    id: 1,
+    question: "What is the capital of Croatia?",
+    options: {
+      A: "Osijek",
+      B: "Zagreb",
+      C: "Rijeka",
+      D: "Split"
+    },
+    answer: "B"
+
+  },
+  {
+    id: 2,
+    question: "Who is the president of South Africa?",
+    options: {
+      A: "Cyril Ramaphosa",
+      B: "Nelson Mandela",
+      C: "Jacob Zuma",
+      D: "Nomazizi Mtshotshisa"
+    },
+    answer: "A"
+  },
+  {
+    id: 3,
+    question: "Which is the most successful team in the UEFA Champions League?",
+    options: {
+      A: "Arsenal",
+      B: "AC Milan",
+      C: "Real Madrid",
+      D: "Liverpool"
+    },
+    answer: "C"
+  },
+  {
+    id: 4,
+    question: "What is the full meaning of CcHub?",
+    options: {
+      A: "Candy Crush Hub",
+      B: "Coca-Cola Hub",
+      C: "Co-Creation Hub",
+      D: "Cameroon Cane Hub"
+    },
+    answer: "C"
+  }
+]
+
+function DisplayQuestionOptions({q_id, options}){
+  var letters = [];
+
+  for(var i in options){
+    letters.push(i);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {letters.map(letter => (
+        <div key={q_id + letter} >
+          <input type="radio" id={q_id + letter} name={q_id} value={letter} />
+          <label> {options[letter]} </label><br></br>
+        </div>
+      ))}
+    </>
+  );
+}
+
+function DisplayAllQuestions({questions}){
+  var q_no=1;
+
+  return (
+    <>
+      {questions.map(qobject => (
+      <div key={qobject.id.toString()} >
+        <h1> {q_no++}) {qobject.question} </h1>
+        <DisplayQuestionOptions q_id={qobject.id} options={qobject.options} />
+      </div>
+      ))}
+    </>
+    
+  );
+}
+
+function ComputeScore({questions}){
+  var score = 0;
+  for(var i=0;i<questions.length;i++){
+    if(document.getElementById(questions[i].id+questions[i].answer).checked){
+      score++
+    }
+  }
+
+  return (
+    <>
+      <h1> Your Score is {score}/4 </h1>
+    </>
+  );
+
+}
+
+
+function App() {
+  const [submit_state,setSubmit] = useState(0);
+
+  if (submit_state === 1){
+    return <ComputeScore questions={questions_arr} />;
+  }
+
+  return (
+    <>
+      <DisplayAllQuestions questions={questions_arr} /><br />
+      <button onClick={() => setSubmit(1)} > Finish </button>
+    </>
   );
 }
 
